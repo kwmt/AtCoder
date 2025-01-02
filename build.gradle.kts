c8d59c2853aa3224c4cbcfd416ad52c2c38f6b8c
Yasutaka Kawamoto <kwmt27@gmail.com>
2025-01-02 14:07:56 +0900

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    kotlin("jvm") version "2.0.20"
+}
+
+group = "net.kwmt27"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(17)
+}
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,5 @@
+package net.kwmt27
+
+fun main() {
+    println("Hello World!")
+}
