e46e03e68b226b05aaef56136d687707fa178942
Yasutaka Kawamoto <kwmt27@gmail.com>
2025-01-18 21:35:44 +0900

    abs389.B

--- src/main/kotlin/net/kwmt27/abc389/B/main.kt
+++ src/main/kotlin/net/kwmt27/abc389/B/main.kt
@@ -1,5 +1,21 @@
-package net.kwmt27.net.kwmt27.abc389.B
+package net.kwmt27.abc389.B
+
+import java.math.BigInteger
 
 fun main() {
+    val X = readln()
 
+    var i: BigInteger = BigInteger.valueOf(1)
+    var nextNumber: BigInteger = BigInteger.valueOf(0)
+    var element: BigInteger = BigInteger.valueOf(1)
+    while (true) {
+        nextNumber = i.plus(BigInteger.valueOf(1))
+        element = element.multiply(nextNumber)
+        if (element.toString() == X) {
+            break
+        }
+        i = i.plus(BigInteger.valueOf(1))
+    }
+
+    println(nextNumber)
 }
---
+++ src/test/kotlin/net/kwmt27/abc389/B/MainKtTest.kt
@@ -0,0 +1,41 @@
+package net.kwmt27.abc389.B
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import java.io.PrintStream
+
+class MainTest {
+    @Test
+    fun sample1() {
+        val input = """
+6""".trimMargin()
+        val output = """
+3""".trimMargin()
+
+        judge(input, output)
+    }
+
+    @Test
+    fun sample2() {
+        val input = """
+2432902008176640000""".trimMargin()
+        val output = """
+20""".trimMargin()
+
+        judge(input, output)
+    }
+
+    private fun judge(input: String, output: String) {
+        val sysIn = ByteArrayInputStream(input.toByteArray())
+        System.setIn(sysIn)
+
+        val sysOut = ByteArrayOutputStream()
+        System.setOut(PrintStream(sysOut))
+
+        main()
+
+        Assertions.assertEquals(output + System.lineSeparator(), sysOut.toString())
+    }
+}
