35241696714c267df2b7f4d4b75867343fd3258f
Yasutaka Kawamoto <kwmt27@gmail.com>
2025-04-27 21:46:48 +0900

    abc403 A

--- src/main/kotlin/net/kwmt27/abc403/A/main.kt
+++ src/main/kotlin/net/kwmt27/abc403/A/main.kt
@@ -1,5 +1,11 @@
 package net.kwmt27.abc403.A
 
 fun main() {
+    val n = readln().toInt()
+    val inputs: List<Int> = readln().split(" ").map { it.toInt() }
 
+    val result = inputs.filterIndexed { index, _ ->
+        (index + 1) % 2 != 0
+    }.sum()
+    println(result)
 }
--- src/test/kotlin/net/kwmt27/abc403/A/MainKtTest.kt
+++ src/test/kotlin/net/kwmt27/abc403/A/MainKtTest.kt
@@ -1,5 +1,54 @@
 package net.kwmt27.abc403.A
 
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import java.io.PrintStream
 
-class MainKtTest
+class MainTest {
+    @Test
+    fun sample1() {
+        val input = """
+7
+3 1 4 1 5 9 2""".trimMargin()
+        val output = """
+14""".trimMargin()
+
+        judge(input, output)
+    }
+
+    @Test
+    fun sample2() {
+        val input = """
+1
+100""".trimMargin()
+        val output = """
+100""".trimMargin()
+
+        judge(input, output)
+    }
+
+    @Test
+    fun sample3() {
+        val input = """
+14
+100 10 1 10 100 10 1 10 100 10 1 10 100 10""".trimMargin()
+        val output = """
+403""".trimMargin()
+
+        judge(input, output)
+    }
+
+    private fun judge(input: String, output: String) {
+        val sysIn = ByteArrayInputStream(input.toByteArray())
+        System.setIn(sysIn)
+
+        val sysOut = ByteArrayOutputStream()
+        System.setOut(PrintStream(sysOut))
+
+        main()
+
+        Assertions.assertEquals(output + System.lineSeparator(), sysOut.toString())
+    }
+}
