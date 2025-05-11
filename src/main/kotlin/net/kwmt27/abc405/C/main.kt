09d4501ebc7ad61055479db85a54a13d1f104dfe
Yasutaka Kawamoto <kwmt27@gmail.com>
2025-05-11 15:14:18 +0900

    405.C

--- src/main/kotlin/net/kwmt27/abc405/C/main.kt
+++ src/main/kotlin/net/kwmt27/abc405/C/main.kt
@@ -11,20 +11,12 @@ fun main() {
 //    i = 2 -> j = 3
 //    i = 3 ->
 
-    var result: BigInteger = BigInteger.ZERO
-    for (i in 1 until N + 1) {
-        for (j in i + 1 until N + 1) {
-            result = result.plus(An[i - 1] * An[j - 1])
-
-            // i = 1のとき
-            // result = result + A1 * A2
-            // result = result + A1 * A3
-
-            // i = 2 のとき
-            // result = result + A2 * A3
-
-        }
+    var ans: BigInteger = BigInteger.ZERO
+    var sum: BigInteger = BigInteger.ZERO
+    for (i in 0 until N) {
+        ans += sum.times(An[i])
+        sum += An[i]
     }
 
-    println(result)
+    println(ans)
 }
